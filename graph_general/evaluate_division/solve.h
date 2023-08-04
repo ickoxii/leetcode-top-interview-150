8de0068ba7f6ec7df50dc11d0ddcc8bfb143460b
ickoxii <80863308+ickoxii@users.noreply.github.com>
2023-08-04 16:44:25 -0700

    evaluate_division completed

--- graph_general/evaluate_division/solve.h
+++ graph_general/evaluate_division/solve.h
@@ -27,9 +27,53 @@ public:
     }
 }; */
 
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        
+        unordered_map<string, vector<pair<string, double>>> graph;
+        // Step 1: Build the graph
+        for (int i = 0; i < equations.size(); ++i) {
+            string numerator = equations[i][0];
+            string denominator = equations[i][1];
+            double quotient = values[i];
+            graph[numerator].push_back({denominator, quotient});
+            graph[denominator].push_back({numerator, 1.0 / quotient});
+        }
+        // Step 2: Process each query
+        vector<double> results;
+        for (const auto& query : queries) {
+            string start = query[0];
+            string end = query[1];
+            if (!graph.count(start) || !graph.count(end)) {
+                // The variable does not exist.
+                results.push_back(-1.0);
+            } else {
+                unordered_set<string> visited;
+                results.push_back(DFS(graph, start, end, visited));
+            }
+        }
+        return results;
+    }
+private:
+    double DFS(unordered_map<string, vector<pair<string, double>>>& graph, string start, string end, unordered_set<string>& visited) {
+        // The end variable is reached
+        if (start == end) return 1.0;
+        visited.insert(start);
+        for (const auto& neighbor : graph[start]) {
+            if (visited.count(neighbor.first) == 0) {
+                double product = DFS(graph, neighbor.first, end, visited);
+                // If the end variable is reached
+                if (product > 0) return product * neighbor.second;
+            }
+        }
+        // If there is no path to the end
+        return -1.0;
     }
 };
