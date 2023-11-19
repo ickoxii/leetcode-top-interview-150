b75a9548bc4ef8a6a762cf2f191de9cd3c32fc81
Icko <80863308+ickoxii@users.noreply.github.com>
2023-11-18 18:43:34 -0800

    more commits

---
+++ trie/design_add_and_search_words_data_structure/solve.h
@@ -0,0 +1,39 @@
+ /**
+  * Design a data structure that supports adding new words and finding
+  * if a string matches any previously added string.
+  *
+  * Implement the WordDictionary class:
+  *
+  * WordDictionary() 
+  *     initializes the object
+  *
+  * void addWord(word)
+  *     adds words to the data structure, it can be matched later
+  *
+  * bool search(word)
+  *     returns true if there is any string in the data structure that
+  *     matches word or false otherwise. word may contain dots '.' where
+  *     dots can be matched with any letter.
+  */
+
+class WordDictionary {
+public:
+    WordDictionary() {
+
+    }
+
+    void addWord(string word) {
+
+    }
+
+    bool search(string word) {
+
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
